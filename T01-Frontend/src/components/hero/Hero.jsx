81b75f4571108efbd6521f0752b7fc4406d2e46f
DevYanezeo <eliseo.yanez@usach.cl>
2025-04-14 12:16:17 -0400

    [Frontend]

---
+++ T01-Frontend/src/components/hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <section className="hero-section">
+      {/* Contenido vacío por ahora */}
+    </section>
+  );
+};
+
+export default Hero;
